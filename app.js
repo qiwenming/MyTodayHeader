6247f8cb040593eef18ec50352087499cd86e3f2
qiwenming <qiwenmingshiwo@163.com>
2017-01-19 18:09:03 +0800

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+//app.js
+App({
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null,
+    baseUrl:'https://v.juhe.cn/toutiao/index?key=b507a3b73efeed900881032d1ed14e51&type='
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,53 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    headerItems:[
+      {id:0, title:'头条' , identy:'top'},
+      {id:1, title:'社会' , identy:'shehui'},
+      {id:2, title:'国内' , identy:'guonei'},
+      {id:3, title:'国际' , identy:'guoji'},
+      {id:4, title:'娱乐' , identy:'yule'},
+      {id:5, title:'体育' , identy:'tiyu'},
+      {id:6, title:'军事' , identy:'junshi'},
+      {id:7, title:'科技' , identy:'keji'},
+      {id:8, title:'财经' , identy:'caijing'},
+      {id:9, title:'时尚' , identy:'shishang'}
+    ],
+    itemIdenty:'top'
+  },
+  onLoad:function(){
+    this.requestHttp('top');
+     wx.setNavigationBarTitle({
+      title: '今日头条-头条'
+    })
+  },
+
+  clickHeaderItem:function(e){
+    console.log(e);
+    var item = e.currentTarget.dataset['item'];
+    if(item.identy==this.itemIdenty){
+      return;
+    }
+    wx.setNavigationBarTitle({
+      title: '今日头条-'+item.title
+    })
+    this.setData({
+      itemIdenty:identy
+    })
+    this.requestHttp(identy);
+  },
+
+  requestHttp:function(newType){
+    var url = app.globalData.baseUrl + newType;
+    console.log(url);
+    wx.request({
+      url: url,
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      success: function(res){
+        console.log(res);
+      }
+    })
+  }
+})
